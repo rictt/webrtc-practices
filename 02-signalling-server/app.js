e26539cd6fc1fa78c80f807b4153d0e199518147
joey <2390923149@qq.com>
2023-11-11 20:08:32 +0800

    feat: one to one

---
+++ 02-signalling-server/app.js
@@ -0,0 +1,155 @@
+const httpServer = require('http').createServer((req, res) => {
+  res.writeHead(200, {
+    "Content-Type": "application/json;charset=utf-8",
+    "access-control-allow-origin": "*"
+  })
+});
+const io = require('socket.io')(httpServer, {
+  cors: {
+    origin: "*"
+  }
+})
+const redis = require('redis')
+
+const redisClient = redis.createClient(6379, '127.0.0.1')
+const roomKey = "metting-room::"
+
+redisClient.on('error', function (error) {
+  console.log('redisCLient error: ', error)
+  console.log('检查是不是redis服务没启动，启动命令为：redis-server')
+})
+
+const userMap = new Map() // user => socket
+
+io.on('connection', async (socket) => {
+  await onListener(socket)
+})
+
+httpServer.listen(18080, async () => {
+  console.log('服务器启动: ' + 'http://localhost:18080')
+  await redisClient.connect();
+  console.log('redis链接成功')
+})
+
+function getMsg(type, msg, status = 200, data = null) {
+  return { type, msg, status, data }
+}
+
+function getParam(url, queryName) {
+  let query = decodeURI(url.split('?')[1]);
+  let vars = query.split("&");
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    if (pair[0] === queryName) {
+      return pair[1];
+    }
+  }
+  return null;
+}
+
+function getUseryDetailByUID(userId, roomId) {
+  let res = JSON.stringify({
+    userId,
+    roomId
+  })
+  return res
+}
+
+async function onListener(socket) {
+  let url = socket.client.request.url
+  let userId = getParam(url, 'userId')
+  let roomId = getParam(url, 'roomId')
+  console.log(url, userId, roomId)
+
+  userMap.set(userId, socket)
+  if (roomId) {
+    await redisClient.hSet(roomKey + roomId, userId + '', getUseryDetailByUID(userId, roomId))
+    oneToMany(roomId, getMsg('join', userId + ' join the room', 200, userId))
+  }
+
+  socket.on('msg', async (data) => {
+    await oneToMany(roomId, data)
+  })
+
+  socket.on('disconnect', async () => {
+    console.log('user: ', userId + ", roomId: " + roomId + " offline!")
+    // userMap.delete(roomKey + roomId, userId)
+    userMap.delete(userId)
+    if (roomId) {
+      await redisClient.hDel(roomKey + roomId, userId)
+      oneToMany(roomId, getMsg('leave', userId + ' leave the room', 200, userId))
+    }
+  })
+
+  socket.on('clean', async (data) => {
+    console.log('c: ', data)
+    await redisClient.del(roomKey + data['roomId'])
+    await redisClient.del(`chat_` + data['roomId'])
+    socket.emit('roomUserList', await getRoomUser(data['roomId']))
+  })
+
+  socket.on('roomUserList', async (data) => {
+    console.log('roomUserList: ', data)
+    socket.emit('roomUserList', await getRoomUser(data['roomId']))
+  })
+
+  socket.on('call', (data) => {
+    let targetUID = data['targetUID']
+    if (userMap.get(targetUID)) {
+      oneToOne(targetUID, getMsg('call', "call", 200, data))
+    } else {
+      console.log('user: ' + targetUID + ' is offline')
+    }
+  })
+
+  socket.on('candidate', (data) => {
+    const targetUID = data['targetUID']
+    if (userMap.get(targetUID)) {
+      oneToOne(targetUID, getMsg('candidate', 'ice candidate', 200, data))
+    } else {
+      console.log('candidate not found: ', targetUID)
+    }
+  })
+
+  socket.on('offer', (data) => {
+    const targetUID = data['targetUID']
+    if (userMap.get(targetUID)) {
+      oneToOne(targetUID, getMsg('offer', 'rtc offer', 200, data))
+    } else {
+      console.log('offer not found: ', targetUID)
+    }
+  })
+
+  socket.on('answer', (data) => {
+    const targetUID = data['targetUID']
+    if (userMap.get(targetUID)) {
+      oneToOne(targetUID, getMsg('answer', 'rtc answer', 200, data))
+    } else {
+      console.log('answer not found: ', targetUID)
+    }
+  })
+
+  socket.on('chat', async (data) => {
+    await redisClient.lPush(`chat_` + roomId, JSON.stringify(data))
+    const list = await redisClient.lRange(`chat_` + roomId, 0, 100000)
+    oneToMany(roomId, getMsg('chat', 'chat', 200, list.map(e => JSON.parse(e))))
+  })
+}
+
+function oneToOne(uid, msg) {
+  let socket = userMap.get(uid)
+  if (socket) {
+    socket.emit('msg', msg)
+  }
+}
+
+async function getRoomUser(roomId) {
+  return await redisClient.hGetAll(roomKey + roomId)
+}
+
+async function oneToMany(roomId, msg) {
+  let uList = await redisClient.hGetAll(roomKey + roomId)
+  for (const uid in uList) {
+    oneToOne(uid, msg)
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # WebRtc Practices
+
+## 命令工具
+- mac查看端口占用：`sudo lsof -i tcp:port`
+- 结束进程：`sudo kill -9 PID`
+
+## one to one大致流程
+
+- 准备信令服务器（交换各自的信息，收集房间、用户信息）
+- 用户携带用户ID和房间ID进行登录
+- 查看房间在线设备
+- 进行呼叫
+  - 初始化本地的video显示
+  - 发送call事件，通过socket转发到对应设备
+  - 初始化PeerConnection实例
+  - 通过pc创建offer，发送offer事件
+- 被呼叫方监听到call事件，处理onCall
+  - 初始化pc对象
+  - 初始化被呼叫方的video显示
+- 被呼叫方监听到offer事件
+  - 创建answer对象，发送answer事件
+- 呼叫方监听到answer
+  - 设置pc对象的remoteDescription
+- 双方监听各自pc对象
+  - 监听ontrack事件，设置remoteVideo
+  - 监听icecandidate，进行协商
